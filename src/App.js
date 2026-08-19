e69186443ee26c082117746339eb3e3c3b9a177c
Egor-as <egor-ashir@yandex.ru>
2026-08-19 22:52:16 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { useEffect } from 'react';
+import './App.css';
+const initData = window.WebApp.initDataUnsafe;
+const mx=window.WebApp;
+if (initData && initData.user) {
+    console.log('ID пользователя:', initData.user.id);
+    console.log('Имя:', initData.user.first_name);
+    console.log('Язык:', initData.user.language_code);
+    console.log('Тип чата:', initData.chat?.type);
+    
+    // Параметр, переданный через диплинк (например, ?startapp=someData)
+    console.log('Параметр запуска:', initData.start_param);
+}
+
+function App() {
+  useEffect(()=>{
+    mx.ready();
+  }, [])
+  const onClose=()=>{
+    mx.close();
+  }
+
+  return (
+    <div className="App">
+      <button onClick={onClose}>Закрыть</button>
+    </div>
+  );
+}
+
+export default App;
