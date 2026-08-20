3ab7c25f894066167ff50b81145e7bb9c7d6d3d1
Egor-as <egor-ashir@yandex.ru>
2026-08-20 14:32:44 +0300

    initial

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';           // глобальные стили (опционально)
 import App from './App';
+import { CartProvider } from './components/CartContext';
 
+// Проверяем, что приложение запущено внутри MAX (для отладки)
+if (window.WebApp) {
+  // Можно выполнить дополнительные действия при готовности WebApp
+  window.WebApp.ready();        // сообщает платформе, что приложение готово
+  console.log('Mini App запущено в MAX');
+} else {
+  console.warn('Приложение запущено вне MAX, некоторые функции могут не работать');
+}
+
+// Создаём корневой элемент React
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Рендерим приложение, обёрнутое в провайдер корзины
 root.render(
   <React.StrictMode>
-    <App />
+    <CartProvider>
+      <App />
+    </CartProvider>
   </React.StrictMode>
 );
